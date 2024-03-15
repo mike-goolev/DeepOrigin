98231e39413f385f8d195cbc676f6dfb4e8e6a2a
mike-goolev <goolevm@gmail.com>
2024-03-14 22:51:01 -0500

    check entire data response

---
+++ cypress/e2e/productAPI/GET_all_products_specs.cy.ts
@@ -0,0 +1,66 @@
+interface ApiResponse {
+    products: {
+        id: number;
+        title: string;
+        description: string;
+        price: number;
+        discountPercentage: number;
+        rating: number;
+        stock: number;
+        brand: string;
+        category: string;
+        thumbnail: string;
+        images: string[];
+    }[];
+    total: number;
+    skip: number;
+    limit: number;
+}
+
+describe("Products API testing", () => {
+    it("GET Tags API", () => {
+        cy.request<ApiResponse>({
+            method: 'GET',
+            url: 'https://dummyjson.com/products'
+        }).then((response) => {
+            cy.log(JSON.stringify(response));
+
+            // Validate status code
+            expect(response.status).to.equal(200);
+
+            // Validate body data response
+            response.body.products.forEach((product, index) => {
+                // Validate propertiy values of the first product[0]
+                if (index === 0) {
+                    expect(product.id).to.equal(1);
+                    expect(product.title).to.equal('iPhone 9');
+                    expect(product.description).to.equal('An apple mobile which is nothing like apple');
+                    expect(product.price).to.equal(549);
+                    expect(product.discountPercentage).to.equal(12.96);
+                    expect(product.rating).to.equal(4.69);
+                    expect(product.stock).to.equal(94);
+                    expect(product.brand).to.equal('Apple');
+                    expect(product.category).to.equal('smartphones');
+                }
+
+                // Validate common properties for all products
+                expect(product.id).to.be.a('number');
+                expect(product.title).to.be.a('string');
+                expect(product.description).to.be.a('string');
+                expect(product.price).to.be.a('number');
+                expect(product.discountPercentage).to.be.a('number');
+                expect(product.rating).to.be.a('number');
+                expect(product.stock).to.be.a('number');
+                expect(product.brand).to.be.a('string');
+                expect(product.category).to.be.a('string');
+                expect(product.thumbnail).to.be.a('string');
+                expect(product.images).to.be.an('array').that.is.not.empty;
+            });
+
+            // Validate other property values
+            expect(response.body.total).to.equal(100);
+            expect(response.body.skip).to.equal(0);
+            expect(response.body.limit).to.equal(30);
+        });
+    });
+});
