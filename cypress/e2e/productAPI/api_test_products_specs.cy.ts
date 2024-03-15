452c7b2499802e994c1a20de02fcd263a9cdf0de
mike-goolev <goolevm@gmail.com>
2024-03-14 22:28:22 -0500

    validating additional properties

--- cypress/e2e/productAPI/api_test_products_specs.cy.ts
+++ cypress/e2e/productAPI/api_test_products_specs.cy.ts
@@ -1,20 +1,25 @@
-interface Product {
-    id: number;
-    title: string;
-    description: string;
-    price: number;
-    discountPercentage: number;
-    rating: number;
-    stock: number;
-    brand: string;
-    category: string;
-    thumbnail: string;
-    images: string[];
+interface ApiResponse {
+    products: {
+        id: number;
+        title: string;
+        description: string;
+        price: number;
+        discountPercentage: number;
+        rating: number;
+        stock: number;
+        brand: string;
+        category: string;
+        thumbnail: string;
+        images: string[];
+    }[];
+    total: number;
+    skip: number;
+    limit: number;
 }
 
 describe("Products API testing", () => {
     it("GET Tags API", () => {
-        cy.request<{ products: Product[] }>({
+        cy.request<ApiResponse>({
             method: 'GET',
             url: 'https://dummyjson.com/products'
         }).then((response) => {
@@ -24,7 +29,7 @@ describe("Products API testing", () => {
             expect(response.status).to.equal(200);
 
             // Validate properties and values of the first product[0]
-            const firstProduct: Product = response.body.products[0];
+            const firstProduct = response.body.products[0];
             expect(firstProduct.id).to.equal(1);
             expect(firstProduct.title).to.equal('iPhone 9');
             expect(firstProduct.description).to.equal('An apple mobile which is nothing like apple');
@@ -34,6 +39,11 @@ describe("Products API testing", () => {
             expect(firstProduct.stock).to.equal(94);
             expect(firstProduct.brand).to.equal('Apple');
             expect(firstProduct.category).to.equal('smartphones');
+
+            // Validate additional properties: total, srip, limit
+            expect(response.body.total).to.equal(100);
+            expect(response.body.skip).to.equal(0);
+            expect(response.body.limit).to.equal(30);
         });
     });
 });
