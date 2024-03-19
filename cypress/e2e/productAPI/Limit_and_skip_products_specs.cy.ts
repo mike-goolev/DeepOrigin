5f966f1e1353253237810beab5197bbdbd9c0768
mike-goolev <goolevm@gmail.com>
2024-03-19 08:41:45 -0500

    added second use case with limit=0

--- cypress/e2e/productAPI/Limit_and_skip_products_specs.cy.ts
+++ cypress/e2e/productAPI/Limit_and_skip_products_specs.cy.ts
@@ -9,16 +9,15 @@ interface LimitAndSkipProductsAPIResponse {
     limit: number;
 }
 
+// Use cace with "limit" and "skip" equal to 10
 describe("Products API testing", () => {
-    it("", () => {
+    it("Passing 'limit' and 'skip' params to limit and skip the results for pagination", () => {
         const limit = 10;
         const skip = 10;
         const selectParams = 'title,price';
 
         cy.request<LimitAndSkipProductsAPIResponse>({
             method: 'GET',
-
-            // passing "limit" and "skip" params to limit and skip the results for pagination
             url: `https://dummyjson.com/products?limit=${limit}&skip=${skip}&select=${selectParams}`
         }).then(response => {
             // Validate response status
@@ -43,6 +42,33 @@ describe("Products API testing", () => {
             // Validate header
             expect(response.headers).to.exist;
             expect(response.headers['content-type']).to.include('application/json');
+        });
+    });
+    
+    // Use cace with "limit"=0
+    it("Test fetching all items with limit=0", () => {
+        const limit = 0;
+        const selectParams = 'title,price';
+
+        cy.request<LimitAndSkipProductsAPIResponse>({
+            method: 'GET',
+            url: `https://dummyjson.com/products?limit=${limit}&select=${selectParams}`
+        }).then(response => {
+            // Validate response status
+            expect(response.status).to.equal(200);
+
+            // Validate response body
+            expect(response.body.products.length).to.equal(response.body.total); 
+            expect(response.body.skip).to.equal(0); 
+            expect(response.body.limit).to.equal(100);
+
+            // Validate selected properties for each product
+            response.body.products.forEach(product => {
+                expect(product).to.have.keys('id', 'title', 'price');
+                expect(product.id).to.be.a('number');
+                expect(product.title).to.be.a('string');
+                expect(product.price).to.be.a('number');
+            });
         });
     });
 });
