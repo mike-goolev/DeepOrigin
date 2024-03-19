f878d28bc5764805e8623818c3b29ea6cd9b800a
mike-goolev <goolevm@gmail.com>
2024-03-19 10:03:35 -0500

    added 201 status check and id value

--- cypress/e2e/productAPI/Post requests/Add_a_new_product_specs.cy.ts
+++ cypress/e2e/productAPI/Post requests/Add_a_new_product_specs.cy.ts
@@ -11,15 +11,15 @@ describe("Products API testing", () => {
             url: 'https://dummyjson.com/products/add',
             headers: { 'Content-Type': 'application/json' },
             body: { title: 'BMW Pencil' }
-
         }).then(response => {
             cy.log(JSON.stringify(response));
 
-            // Validate response status
-            expect(response.status).to.equal(200);
+            // Validate response status - why 201 is failing after POST here????
+            expect(response.status).to.equal(201);
 
             // Validate product data
             expect(response.body.title).to.equal('BMW Pencil');
+            expect(response.body.id).to.equal(101);
         });
     });
 });
